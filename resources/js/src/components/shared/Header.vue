d7f6ee9e202223545b5d2a6f39efe8e340793b67
Murat Karabacak <m9urat@gmail.com>
2021-07-14 17:29:55 +0300

    added:navbar links

--- resources/js/src/components/shared/Header.vue
+++ resources/js/src/components/shared/Header.vue
@@ -194,55 +194,12 @@
             <div class="container">
                 <nav class="main-nav">
                     <ul class="menu sf-arrows">
-                        <li class="active"><a href="index-2.html">Home</a></li>
-                        <li>
-                            <a href="category.html" class="sf-with-ul">Categories</a>
-                            <div class="megamenu megamenu-fixed-width">
-                                <div class="row">
-                                    <div class="col-lg-8">
-                                        <div class="row">
-                                            <div class="col-lg-6">
-                                                <div class="menu-title">
-                                                    <a href="#">Variations 1<span class="tip tip-new">New!</span></a>
-                                                </div>
-                                                <ul>
-                                                    <li><a href="category-banner-full-width.html">Fullwidth Banner<span class="tip tip-hot">Hot!</span></a></li>
-                                                    <li><a href="category-banner-boxed-slider.html">Boxed Slider Banner</a></li>
-                                                    <li><a href="category-banner-boxed-image.html">Boxed Image Banner</a></li>
-                                                    <li><a href="category-sidebar-left.html">Left Sidebar</a></li>
-                                                    <li><a href="category-sidebar-right.html">Right Sidebar</a></li>
-                                                    <li><a href="category-flex-grid.html">Product Flex Grid</a></li>
-                                                    <li><a href="category-horizontal-filter1.html">Horizontal Filter1</a></li>
-                                                    <li><a href="category-horizontal-filter2.html">Horizontal Filter2</a></li>
-                                                </ul>
-                                            </div><!-- End .col-lg-6 -->
-                                            <div class="col-lg-6">
-                                                <div class="menu-title">
-                                                    <a href="#">Variations 2</a>
-                                                </div>
-                                                <ul>
-                                                    <li><a href="#">Product List Item Types</a></li>
-                                                    <li><a href="category-infinite-scroll.html">Ajax Infinite Scroll</a></li>
-                                                    <li><a href="category-3col.html">3 Columns Products</a></li>
-                                                    <li><a href="category-4col.html">4 Columns Products <span class="tip tip-new">New</span></a></li>
-                                                    <li><a href="category-5col.html">5 Columns Products</a></li>
-                                                    <li><a href="category-6col.html">6 Columns Products</a></li>
-                                                    <li><a href="category-7col.html">7 Columns Products</a></li>
-                                                    <li><a href="category-8col.html">8 Columns Products</a></li>
-                                                </ul>
-                                            </div><!-- End .col-lg-6 -->
-                                        </div><!-- End .row -->
-                                    </div><!-- End .col-lg-8 -->
-                                    <div class="col-lg-4">
-                                        <div class="banner">
-                                            <a href="#">
-                                                <img src="assets/images/menu-banner-2.jpg" alt="Menu banner">
-                                            </a>
-                                        </div><!-- End .banner -->
-                                    </div><!-- End .col-lg-4 -->
-                                </div>
-                            </div><!-- End .megamenu -->
-                        </li>
+                        <router-link :to="{ name : 'home' }" tag="li" active-class="active">
+                            <a>Home</a>
+                        </router-link>
+                        <router-link :to="{ name : 'categories' }" tag="li" active-class="active">
+                            <a>Kategoriler</a>
+                        </router-link>
                         <li class="megamenu-container">
                             <a href="product.html" class="sf-with-ul">Products</a>
                             <div class="megamenu">
